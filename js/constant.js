244606fd51b1d44e9f686f23e829ef6eef0a523d
Satoru Kurita <kurita@lvi.co.jp>
2025-09-21 20:57:07 +0900

    WIP

--- js/constant.js
+++ js/constant.js
@@ -2,4 +2,4 @@
 export const APP_BASE_URL = "https://shoei-549678050196.asia-northeast1.run.app/";
 //export const APP_BASE_URL = "https://localhost:7070/"
 export const ATTENDANCE_TOGGLE_API_ENDPOINT = `${APP_BASE_URL}api/attendance`;
-export const LOGIN_END_POINT = APP_BASE_URL + "api/user_login";
+export const LOGIN_END_POINT = APP_BASE_URL + "api/userLoginFromSmartPhone";
