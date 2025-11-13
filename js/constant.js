ba1e90a3e9f2deeaca3d7bd196607b340791a9c1
satoru <rhodeus208@gmail.com>
2025-11-13 14:59:15 +0900

    fixed url for prod

--- js/constant.js
+++ js/constant.js
@@ -1,7 +1,7 @@
 // TODO: come up with some idea to not hard code this
-// export const APP_BASE_URL =
-//   "https://shoei-549678050196.asia-northeast1.run.app/";
-export const APP_BASE_URL = " https://efd04a35f759.ngrok-free.app/";
+export const APP_BASE_URL =
+  "https://shoei-549678050196.asia-northeast1.run.app/";
+// export const APP_BASE_URL = " https://efd04a35f759.ngrok-free.app/";
 // export const APP_BASE_URL = "https://localhost:8080/"
 export const ATTENDANCE_TOGGLE_API_ENDPOINT = `${APP_BASE_URL}api/attendance`;
 export const LOGIN_END_POINT = APP_BASE_URL + "api/userLoginFromSmartPhone";
